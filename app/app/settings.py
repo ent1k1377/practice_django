2e6d59fe140d7f0b70aa6de72fed96413ac7a320
DESKTOP-RC54URQ\ent1k <ent1k1337@yandex.ru>
2023-01-25 02:01:20 +0300

    added migrates, added static and media settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -115,10 +115,25 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS =[
+
+]
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
+# MEDIA
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
